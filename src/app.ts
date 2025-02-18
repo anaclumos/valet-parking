f502c1099d8a1a2025e49f2b7e6e1570672e8d4b
Sunghyun Cho <hey@cho.sh>
2025-02-19 02:26:19 +0900

    feat: add logging for opened issues

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { Context, Probot } from "probot"
 
 export default (app: Probot) => {
   app.on("issues.opened", async (context: Context) => {
+    console.log("Issue Opened:", JSON.stringify(context))
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     })
