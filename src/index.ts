2412c31d26d88a46a9fe4bb22c6da8bb802ecc12
Sunghyun Cho <hey@cho.sh>
2025-02-19 01:24:37 +0900

    style: remove unnecessary semicolons and commas

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { Probot,Context } from "probot";
+import { Context, Probot } from "probot"
 
 export default (app: Probot) => {
   app.on("issues.opened", async (context: Context) => {
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
+    })
+    await context.octokit.issues.createComment(issueComment)
+  })
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-};
+}
